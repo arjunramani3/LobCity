2eb8bdc5001a538b3771ba80fb09c0574e157fd6
Arjun Ramani <arjunramani3@gmail.com>
2019-02-22 18:54:10 -0800

    fix errors

--- dataImport/compas1.R
+++ dataImport/compas1.R
@@ -1,7 +1,9 @@
 library(RCurl)
-libary(lubridate)
-url <- 'https://raw.githubusercontent.com/arjunramani3/compas-analysis/master/compas-scores-two-years.csv'
+library(tidyverse)
+url1 <- 'https://raw.githubusercontent.com/arjunramani3/compas-analysis/master/compas-scores-two-years.csv'
+url2 <- 'https://raw.githubusercontent.com/arjunramani3/compas-analysis/master/compas-scores.csv'
 
-df <- read_csv(url)
-
-
+compas2Year <- read_csv(url1)
+compas <- read_csv(url2)
+write_csv(compas2Year, 'compas2Year.csv')
+write_csv(compas, 'compas.csv')
