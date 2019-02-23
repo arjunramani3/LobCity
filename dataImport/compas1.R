94608978b5d3f3f8f6867a202ea48c908ad1b00d
Arjun Ramani <arjunramani3@gmail.com>
2019-02-23 11:28:45 -0800

    preprocessed data (trimmed.csv)

--- dataImport/compas1.R
+++ dataImport/compas1.R
@@ -17,3 +17,21 @@ con = dbConnect(RSQLite::SQLite(), dbname="compas.db")
 alltables = dbListTables(con)
 prison = dbGetQuery(con,'select * from prisonhistory' )
 people = dbGetQuery(con,'select * from people' )
+both = merge(people, prison, by=c('first', 'last'))
+
+write.csv(both, 'merged1.csv')
+
+
+####PREPROCESSING
+both$in_cust_date <- as.Date(both$in_custody)
+both$out_cust_date <- as.Date(both$out_custody)
+both$prison_length <- both$out_cust_date - both$in_cust_date
+both$prison_length
+
+library(dplyr)
+trim <- select(both, c(sex, race, age, juv_fel_count, juv_misd_count, juv_other_count,
+                  priors_count, c_charge_degree, c_charge_desc, is_recid, 
+                  num_r_cases, r_charge_degree, r_charge_desc, is_violent_recid,
+                  prison_length))
+trim2 <- select(both, c(age, juv_fel_count, juv_misd_count, juv_other_count,
+                        priors_count, c_charge_degree, c_charge_desc, is_recid, prison_length))
